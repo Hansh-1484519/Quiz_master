31655313d4b5774d7762b673e83e6b15fd04a006
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-13 08:49:27 +0530

    added dom manipulation to app

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+// Grabing reference to all element
+const heading = document.querySelector('h1');
+const quizContainer = document.getElementById('quiz-container');
+const submitButton = document.getElementById('submit');
+const result = document.getElementById('result');
 
-const quizContainer = document.getElementById("quiz-container");
-quizContainer.onmouseenter = 
-    () => {
+// function to display quizes
+function quizDisplay (){
 
-        quizContainer.style.backgroundColor = "white";
-    }
-quizContainer.onmouseleave = function colorChange () {
-    quizContainer.style.backgroundColor = "yellow";
+};
+
+// Handle submitting the quiz
+submitButton.addEventListener('click' , () => {});
+
+// Display result 
+function DisplayResult () {
+    
 }
+
+
+
+heading.style.marginBottom = " 5px ";
+heading.style.borderBottom = "2px solid green"
+result.style.backgroundColor = "purple";
+
