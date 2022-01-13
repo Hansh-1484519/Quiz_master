fa5700044dbbe7100bb184f8b638b4f683bfbd4b
Hansh Raj Sharma <hanshrajsharmamax360@gmail.com>
2022-01-13 13:30:51 +0530

    updated script.js to represent questions in form of array of object

--- script.js
+++ script.js
@@ -7,15 +7,73 @@ const result = document.getElementById('result');
 // function to display quizes
 function quizDisplay (){
 
-};
+    // Array of questions' 
+    const quizQuestions = [
+        {
+            question : "How many days are there in a week ?",
+            answer : {
+                a : "6",
+                b : "7",
+                c : "8",
+                d : "9"
+            },
+            correctAnswer : "b"
+        },
+        {
+            question : "How many hours are there in a Day ?",
+            answer : {
+                a : "24",
+                b : "44",
+                c : "14",
+                d : "18"
+            },
+            correctAnswer : "a"
+        },
+        {
+            question : "How many days are there in a leep Year ?",
+            answer : {
+                a : "365",
+                b : "364",
+                c : "363",
+                d : "366"
+            },
+            correctAnswer : "d"
+        },
+        {
+            question : "How many days are there in a july month?",
+            answer : {
+                a : "29",
+                b : "30",
+                c : "31",
+                d : "32"
+            },
+            correctAnswer : "c"
+        }
+    ];
+    quizQuestions.forEach( (e) => {
+        console.log(e.question);
 
-// Handle submitting the quiz
-submitButton.addEventListener('click' , () => {});
+        console.log(e.answer);
+              console.log(e.answer.a);
+              console.log(e.answer.b);
+              console.log(e.answer.c);
+              console.log(e.answer.d);
+        console.log(e.correctAnswer);
+    })
+
+
+};
 
 // Display result 
 function DisplayResult () {
     
 }
+
+// display Quiz right away
+quizDisplay();
+
+// Handle submitting the quiz
+submitButton.addEventListener('click' ,  DisplayResult);
 
 
 
